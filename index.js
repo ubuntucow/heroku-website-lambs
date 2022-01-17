a69a889036229dea97b59c9d7ee7d84550f5b079
ubuntucow <adamypadan@gmail.com>
2022-01-17 18:26:03 +0000

    Setup heroku

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const path = require("path");
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
